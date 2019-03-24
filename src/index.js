73d8d5d263d688da96732a916455a04e6f3411f3
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-24 15:25:03 -0500

    Ajustes2

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase';
 
-// Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyDUaQAg1lvL0ufkLf8udHXpCnIoXsKT8Dc',
-  authDomain: 'pseudogram-d1e0d.firebaseapp.com',
-  databaseURL: 'https://pseudogram-d1e0d.firebaseio.com',
-  projectId: 'pseudogram-d1e0d',
-  storageBucket: 'pseudogram-d1e0d.appspot.com',
-  messagingSenderId: '201330211059',
-};
-firebase.initializeApp(config);
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
